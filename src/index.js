084b3ace241bd68e43ce2ff75434d3f45840e813
CuB21 <igor.yakubovsky@gmail.com>
2020-03-23 23:14:49 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined || array.length === 0) {
+    return 0;
+  }
+  array.sort((a,b) => {
+    return a - b;
+  })
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined || array.length === 0) {
+    return 0;
+  }
+  array.sort((a,b) => {
+    return b - a;
+  })
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array===undefined || array.length === 0) {
+    return 0;
+  }
+  let num = 0;
+  array.forEach((element) => {
+    num += element;
+  })
+  return num/array.length;
 }
